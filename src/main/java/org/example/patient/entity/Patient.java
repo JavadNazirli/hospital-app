68d9708e58141d23346777bdcbdc726959ef2e85
javad <javadnazirli24@gmail.com>
2025-08-03 03:37:32 +0400

    Patient entity was structured

--- src/main/java/org/example/patient/entity/Patient.java
+++ src/main/java/org/example/patient/entity/Patient.java
@@ -1,4 +1,29 @@
 package org.example.patient.entity;
 
+import lombok.*;
+import org.example.common.model.enums.Gender;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+@Builder
 public class Patient {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Gender gender;
+    private String nationalId;
+    private String phone;
+    private String email;
+    private String address;
+    private String emergencyContactName;
+    private String emergencyContactPhone;
+    private boolean isActive;
+    private LocalDateTime createdAt;
 }
