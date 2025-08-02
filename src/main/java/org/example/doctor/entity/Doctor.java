f1e4cdb3198f782c38d2a502aab1edc461521541
javad <javadnazirli24@gmail.com>
2025-08-03 03:45:49 +0400

    Doctor entity was structured

--- src/main/java/org/example/doctor/entity/Doctor.java
+++ src/main/java/org/example/doctor/entity/Doctor.java
@@ -1,4 +1,24 @@
 package org.example.doctor.entity;
 
+import lombok.*;
+import org.example.common.model.enums.Branches;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Doctor {
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private Branches branch;
+  private String email;
+  private String phone;
+  private String roomNumber;
+  private boolean isActive;
+  private LocalDateTime createdAt;
 }
