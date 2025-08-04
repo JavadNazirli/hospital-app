153c4d401b393a956491427d83acf526a9d4e68c
javad <javadnazirli24@gmail.com>
2025-08-04 17:52:23 +0400

    AppointmentRepository, PatientRepository and DoctorRepository were structured

--- src/main/java/org/example/appointment/repository/AppointmentRepository.java
+++ src/main/java/org/example/appointment/repository/AppointmentRepository.java
@@ -1,4 +1,15 @@
 package org.example.appointment.repository;
 
-public interface AppointmentRepository {
+import org.example.appointment.entity.Appointment;
+import org.example.common.base.BaseRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AppointmentRepository extends BaseRepository<Appointment, Long> {
+    List<Appointment> findByDoctor(Long doctorId);
+
+    List<Appointment> findByPatient(Long patientId);
+
+    List<Appointment> findByDate(LocalDate date);
 }
--- src/main/java/org/example/doctor/repository/DoctorRepository.java
+++ src/main/java/org/example/doctor/repository/DoctorRepository.java
@@ -1,4 +1,13 @@
 package org.example.doctor.repository;
 
-public interface DoctorRepository {
+import org.example.common.base.BaseRepository;
+import org.example.common.model.enums.Branches;
+import org.example.doctor.entity.Doctor;
+
+import java.util.List;
+
+public interface DoctorRepository extends BaseRepository<Doctor, Long> {
+    List<Doctor> findByBranch(Branches branch);
+
+    boolean existsByNationalId(String nationalId);
 }
