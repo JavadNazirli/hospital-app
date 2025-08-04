67f2e409560459b734d421d0e6f4e88588879b58
javad <javadnazirli24@gmail.com>
2025-08-04 17:53:37 +0400

    Branches was structured

--- src/main/java/org/example/common/model/enums/Branches.java
+++ src/main/java/org/example/common/model/enums/Branches.java
@@ -1,4 +1,14 @@
 package org.example.common.model.enums;
 
 public enum Branches {
+    CARDIOLOGY,
+    NEUROLOGY,
+    DERMATOLOGY,
+    PEDIATRICS,
+    ORTHOPEDICS,
+    GENERAL_SURGERY,
+    INTERNAL_MEDICINE,
+    GYNECOLOGY,
+    UROLOGY,
+    RADIOLOGY
 }
